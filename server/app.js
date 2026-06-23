7c5228125388dc7628175bfc6bc80fc34a265dbc
Sankalp-Space <sankalpshrivastav2001@gmail.com>
2026-06-23 23:44:27 +0530

    cors changes

--- server/app.js
+++ server/app.js
@@ -10,7 +10,10 @@ const app = express()
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+    origin: [
+      'https://ajaia-docss.vercel.app',
+      'http://localhost:5173',
+    ],
     credentials: true,
   })
 )
